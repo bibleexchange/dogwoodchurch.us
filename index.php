27a342417fc94cc0559392b208e2446b10e04d31
mjamesderocher <mjamesderocher@gmail.com>
2016-02-25 17:16:54 -0500

    Update

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
+<!DOCTYPE html>
 <html>
 <head>
-	<title>Holiness Christian Academy</title>
+	<title>Dogwood Church of Elkton, Maryland</title>
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="HandheldFriendly" content="true" />
 	<meta name="viewport" content="width=device-width, initial-scale=1">
@@ -9,56 +10,152 @@
 	
 </head>
 <body>
-	
+
+<script>
+    $(function() {
+	  $('a[href*="#"]:not([href="#"])').click(function() {
+	    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+	      var target = $(this.hash);
+	      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+	      if (target.length) {
+	        $('html, body').animate({
+	          scrollTop: target.offset().top
+	        }, 1000);
+	        return false;
+	      }
+	    }
+	  });
+	});
+</script>
+<!--Facebook SDK information-->	
 	<div id="fb-root"></div>
 	
-	<script>(function(d, s, id) {
-	  var js, fjs = d.getElementsByTagName(s)[0];
-	  if (d.getElementById(id)) return;
-	  js = d.createElement(s); js.id = id;
-	  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.3&appId=1529479753993292";
-	  fjs.parentNode.insertBefore(js, fjs);
-	}(document, 'script', 'facebook-jssdk'));</script>
+	<script>
+	  window.fbAsyncInit = function() {
+	    FB.init({
+	      appId      : '1683706391887162',
+	      xfbml      : true,
+	      version    : 'v2.5'
+	    });
+	  };
 	
-
+	  (function(d, s, id){
+	     var js, fjs = d.getElementsByTagName(s)[0];
+	     if (d.getElementById(id)) {return;}
+	     js = d.createElement(s); js.id = id;
+	     js.src = "//connect.facebook.net/en_US/sdk.js";
+	     fjs.parentNode.insertBefore(js, fjs);
+	   }(document, 'script', 'facebook-jssdk'));
+	</script>
 	
-	<div class="container-fluid">
-		<?php include('partials/navbar.html'); ?>
+	
+	
+<!--End Facebook information-->
+	
+<div class="hero-banner">
+	<div class="blur">
+	<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0" y="0" viewBox="0, 0, 576, 201.6" id="hero-logo">
+	  <g id="Layer_1">
+	    <path d="M25.649,20.286 L9.628,102.947 C9.628,102.947 44.804,102.902 58.098,102.269 C102.092,107.017 131.213,21.398 72.81,21.056 C48.608,19.701 25.649,20.286 25.649,20.286 z" fill="#D20C07"/>
+	    <path d="M7.352,114.649 L55.952,114.649 C66.302,114.649 75.302,113.149 83.852,109.249 C105.752,99.499 119.852,75.499 119.852,50.749 C119.852,31.099 111.152,18.199 95.552,12.199 C88.502,9.799 81.302,8.749 71.702,8.749 L27.902,8.749 L25.652,20.299 L38.852,20.299 L22.802,102.949 L9.602,102.949 z M37.802,102.349 L53.702,21.049 L70.802,21.049 C77.852,21.049 83.702,21.799 88.802,24.049 C98.852,28.399 104.552,37.699 104.552,51.949 C104.552,71.449 94.052,89.449 78.302,97.549 C71.402,100.849 64.052,102.349 55.052,102.349 z" fill="#113C89"/>
+	    <path d="M117.582,89.329 C117.582,105.169 129.102,116.089 145.542,116.089 C165.222,116.089 183.822,100.129 183.822,79.009 C183.822,63.289 172.302,52.489 155.862,52.489 C136.062,52.489 117.582,68.449 117.582,89.329 z M129.462,88.849 C129.462,73.969 142.182,62.569 154.902,62.569 C164.982,62.569 171.942,69.529 171.942,79.609 C171.942,94.609 159.102,106.009 146.502,106.009 C136.422,106.009 129.462,99.049 129.462,88.849 z" fill="#113C89"/>
+	    <path d="M188.502,126.049 L183.582,134.449 C186.822,136.249 194.142,139.849 205.422,139.849 C219.822,139.849 235.062,132.769 239.022,112.729 L248.142,65.329 C248.502,63.769 249.702,62.929 251.262,62.929 L257.982,62.929 L259.662,53.929 L246.582,53.929 C241.662,53.929 239.262,56.329 238.542,60.169 C238.182,61.849 238.302,62.689 238.302,62.689 L238.062,62.689 C236.742,57.529 230.262,52.489 219.342,52.489 C198.702,52.489 187.302,72.409 187.302,89.329 C187.302,103.729 195.582,113.929 210.462,113.929 C218.742,113.929 224.742,110.209 229.182,103.969 L229.422,103.969 C229.422,103.969 228.582,106.249 227.622,110.809 C224.982,125.329 215.622,130.009 204.702,130.009 C195.102,130.009 188.502,126.049 188.502,126.049 z M199.182,88.489 C199.182,75.649 206.742,62.569 220.302,62.569 C227.982,62.569 234.222,65.689 234.222,75.649 C234.222,87.889 227.382,103.849 213.222,103.849 C204.102,103.849 199.182,97.849 199.182,88.489 z" fill="#113C89"/>
+	    <path d="M274.222,114.649 L287.182,114.649 L307.822,74.089 C309.142,71.329 309.982,68.329 309.982,68.329 L310.342,68.329 C310.342,68.329 310.102,71.329 310.342,74.089 L315.142,114.649 L328.222,114.649 L353.902,62.929 L361.462,62.929 L363.262,53.929 L335.662,53.929 L333.982,62.929 L342.142,62.929 L324.862,98.209 C323.662,100.729 322.822,103.849 322.822,103.849 L322.462,103.849 C322.462,103.849 322.822,100.729 322.582,98.209 L318.022,54.049 L307.342,54.049 L285.262,98.209 C284.182,100.729 283.342,103.849 283.342,103.849 L282.982,103.849 C282.982,103.849 283.342,100.729 283.102,98.209 L279.622,62.929 L287.902,62.929 L289.582,53.929 L262.822,53.929 L261.022,62.929 L268.582,62.929 z" fill="#113C89"/>
+	    <path d="M355.262,89.329 C355.262,105.169 366.782,116.089 383.222,116.089 C402.902,116.089 421.502,100.129 421.502,79.009 C421.502,63.289 409.982,52.489 393.542,52.489 C373.742,52.489 355.262,68.449 355.262,89.329 z M367.142,88.849 C367.142,73.969 379.862,62.569 392.582,62.569 C402.662,62.569 409.622,69.529 409.622,79.609 C409.622,94.609 396.782,106.009 384.182,106.009 C374.102,106.009 367.142,99.049 367.142,88.849 z" fill="#113C89"/>
+	    <path d="M425.502,89.329 C425.502,105.169 437.022,116.089 453.462,116.089 C473.142,116.089 491.742,100.129 491.742,79.009 C491.742,63.289 480.222,52.489 463.782,52.489 C443.982,52.489 425.502,68.449 425.502,89.329 z M437.382,88.849 C437.382,73.969 450.102,62.569 462.822,62.569 C472.902,62.569 479.862,69.529 479.862,79.609 C479.862,94.609 467.022,106.009 454.422,106.009 C444.342,106.009 437.382,99.049 437.382,88.849 z" fill="#113C89"/>
+	    <path d="M494.862,91.249 C494.862,106.369 503.022,116.089 517.662,116.089 C526.782,116.089 534.942,110.449 539.022,103.969 L539.262,103.969 C539.022,112.129 544.422,114.889 552.102,114.889 C554.502,114.889 557.142,114.649 557.142,114.649 L558.822,105.649 C558.822,105.649 557.742,105.889 556.062,105.889 C553.062,105.889 550.542,104.929 550.542,100.609 C550.542,99.529 550.782,98.209 551.022,96.889 L567.878,8.663 L546.158,8.663 L544.358,17.663 L554.438,17.663 L546.822,58.489 C546.342,60.649 546.222,62.929 546.222,62.929 L545.982,62.929 C543.222,55.609 535.902,52.489 528.342,52.489 C510.822,52.489 494.862,68.209 494.862,91.249 z M506.742,90.409 C506.742,73.009 518.622,62.689 529.062,62.689 C538.542,62.689 542.622,69.649 542.622,78.049 C542.622,91.969 533.142,106.009 520.422,106.009 C511.902,106.009 506.742,100.129 506.742,90.409 z" fill="#113C89"/>
+	    <path d="M270.471,161.527 C270.471,175.382 281.266,183.967 293.676,183.967 C302.771,183.967 308.976,179.122 310.931,177.337 L307.786,171.302 C306.256,172.492 301.326,176.827 294.016,176.827 C285.771,176.827 278.886,171.132 278.886,161.357 C278.886,152.177 284.836,146.057 293.506,146.057 C297.331,146.057 302.771,147.672 302.771,150.987 L302.771,153.962 L310.081,153.962 L310.081,148.692 C310.081,141.297 299.626,138.917 293.251,138.917 C280.416,138.917 270.471,148.352 270.471,161.527 z" fill="#D20C07"/>
+	    <path d="M314.796,182.947 L336.556,182.947 L336.556,176.572 L329.756,176.572 L329.756,162.802 C329.756,153.622 335.706,146.482 344.121,146.482 C351.006,146.482 352.281,151.242 352.281,157.277 L352.281,182.947 L367.241,182.947 L367.241,176.572 L360.526,176.572 L360.526,155.407 C360.526,144.442 355.851,138.917 345.736,138.917 C337.151,138.917 331.541,144.612 329.671,148.777 L329.501,148.777 C329.501,148.777 329.756,147.247 329.756,145.207 L329.756,122.937 L314.371,122.937 L314.371,129.312 L321.596,129.312 L321.596,176.572 L314.796,176.572 z" fill="#D20C07"/>
+	    <path d="M374.696,167.477 C374.696,178.867 379.541,183.967 388.806,183.967 C399.516,183.967 404.361,176.062 405.211,173.767 L405.381,173.767 C405.381,173.767 405.211,174.872 405.211,176.317 L405.211,178.187 C405.211,181.247 406.911,182.947 410.651,182.947 L419.576,182.947 L419.576,176.657 L415.071,176.657 C413.881,176.657 413.201,175.977 413.201,174.787 L413.201,139.937 L397.731,139.937 L397.731,146.312 L404.956,146.312 L404.956,159.742 C404.956,168.242 399.601,176.487 391.016,176.487 C384.131,176.487 382.941,171.727 382.941,165.777 L382.941,139.937 L367.471,139.937 L367.471,146.312 L374.696,146.312 z" fill="#D20C07"/>
+	    <path d="M424.546,182.947 L445.626,182.947 L445.626,176.572 L439.251,176.572 L439.251,165.182 C439.251,156.767 443.161,147.502 452.341,147.502 C453.701,147.502 454.721,147.757 454.721,147.757 L454.721,139.597 C454.721,139.597 453.701,139.427 452.596,139.427 C445.966,139.427 440.951,144.357 438.996,150.647 L438.826,150.647 C438.826,150.647 438.996,149.372 438.996,147.672 L438.996,144.952 C438.996,141.467 437.211,139.937 433.471,139.937 L423.781,139.937 L423.781,146.312 L429.136,146.312 C430.326,146.312 431.006,146.907 431.006,148.182 L431.006,176.572 L424.546,176.572 z" fill="#D20C07"/>
+	    <path d="M457.226,161.527 C457.226,175.382 468.021,183.967 480.431,183.967 C489.526,183.967 495.731,179.122 497.686,177.337 L494.541,171.302 C493.011,172.492 488.081,176.827 480.771,176.827 C472.526,176.827 465.641,171.132 465.641,161.357 C465.641,152.177 471.591,146.057 480.261,146.057 C484.086,146.057 489.526,147.672 489.526,150.987 L489.526,153.962 L496.836,153.962 L496.836,148.692 C496.836,141.297 486.381,138.917 480.006,138.917 C467.171,138.917 457.226,148.352 457.226,161.527 z" fill="#D20C07"/>
+	    <path d="M502.551,182.947 L524.311,182.947 L524.311,176.572 L517.511,176.572 L517.511,162.802 C517.511,153.622 523.461,146.482 531.876,146.482 C538.761,146.482 540.036,151.242 540.036,157.277 L540.036,182.947 L554.996,182.947 L554.996,176.572 L548.281,176.572 L548.281,155.407 C548.281,144.442 543.606,138.917 533.491,138.917 C524.906,138.917 519.296,144.612 517.426,148.777 L517.256,148.777 C517.256,148.777 517.511,147.247 517.511,145.207 L517.511,122.937 L502.126,122.937 L502.126,129.312 L509.351,129.312 L509.351,176.572 L502.551,176.572 z" fill="#D20C07"/>
+	    <path d="M38.813,20.286 L53.732,21.041 L37.873,102.337 L22.748,102.947 L38.813,20.286 z" fill="#E2CA04"/>
+	    <path d="M21.138,43.541 L64.066,43.537 L61.378,57.159 L18.45,57.142 L21.138,43.541 z" fill="#E2CA04"/>
+	  </g>
+	</svg>
+	<h1>Dogwood Church</h1>
+	<h2>Elkton, Maryland</h2>
+	<a href="#about" id="down-arrows">
+		<svg  version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0" y="0" viewBox="0, 0, 360, 80">
+		  <g id="dwn">
+		    <path d="M0.5,33 L180.5,78 L360.5,33" fill-opacity="0" stroke-width="2"/>
+		    <path d="M0.5,3 L180.5,48 L360.5,3" fill-opacity="0" stroke-width="2"/>
+		  </g>
+		</svg>
+	</a>
 	</div>
-	
-<div class="wrapper">	
-
-    <div class="container">
-        
-		<?php include('partials/headline.html'); ?>
-		
-		<div class="row">
-			
-			<div class="col-sm-8">
-				<?php include('partials/curriculum.html'); ?>
-				<?php include('partials/staff.html'); ?>
-				<?php include('partials/reputation.html'); ?>
-			</div>
-			
-			<div class="col-sm-4">
-				<?php include('partials/facebook-notifications.html'); ?>
-			</div>	
-			
-			<div class="col-sm-8">
-				<?php include('partials/location.html'); ?>
-			</div>
-			
-		</div>
-		
-
-		
-    </div>
-    
-	<div class="push"><!--//--></div>
 </div>
 
-	<?php include('partials/footer.html'); ?>
-		
+<div class="container-fluid">
+	
+	<section class="module row parallax parallax-1">	
+		<div class="col-md-5 col-md-offset-1">	
+			<div class="content-square">
+				<h2>Service Times</h2>
+				<ul>
+					<li>Sunday 10 am</li>
+					<li>Sunday 6 pm</li>
+					<li>Wednesday 7 pm</li>
+				</ul>
+			</div>
+		</div>
+		<div class="col-md-5">
+			<div class="content-square">
+				<h2 id="about">About Us</h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ultricies justo eu scelerisque ullamcorper. In vitae mauris dapibus dolor ultrices egestas. Nam hendrerit, tellus ullamcorper hendrerit ultricies, purus mauris faucibus nulla, et pretium libero libero vitae lorem. Morbi finibus felis bibendum, faucibus ante et, tempus lectus. Aliquam ultrices nisl id magna consectetur eleifend. Mauris nec nunc dignissim, malesuada massa non, vestibulum nisi. Nulla consequat enim at congue tempus. In eu nisi nec felis luctus rutrum.</p>
+
+<p>Aenean dignissim euismod porttitor. Nunc suscipit gravida nulla eu luctus. Maecenas non dapibus diam. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Fusce quis massa sit amet ex congue euismod. Mauris et interdum felis, vitae vehicula ex. Nunc vel vestibulum neque. Etiam sodales vehicula nisi eget eleifend. Ut eu sapien est. Nunc at sem tortor. Fusce et nisi nulla.</p>
+				
+			</div>
+		</div>
+	</section>
+	<div class="row">
+		<h2>Audio Sermons</h2>
+		<div class="col-md-10 col-md-offset-1">
+			<iframe width="100%" height="350" style="background-color:transparent; display:block;" frameborder="0" allowtransparency="allowtransparency" scrolling="no" src="//embeds.audioboom.com/publishing/playlist/v4?bg_fill_col=%23ecefef&amp;boo_content_type=playlist&amp;data_for_content_type=1311022-moments-of-christian-history&amp;image_option=small&amp;link_color=%2358d1eb&amp;player_theme=light&amp;src=https%3A%2F%2Fapi.audioboom.com%2Fplaylists%2F1311022-moments-of-christian-history" title="audioBoom player"></iframe>
+		</div>
+	</div>
+	<section class="module row parallax parallax-2">
+		<div class="frosted">
+			<h2>Events</h2>
+			<div class="col-md-10 col-md-offset-1">
+				<div class="row">
+					<?php include 'partials/fb-events.php'; ?>
+				</div>
+			</div>
+		</div>
+	</section>
+	<div class="row">
+		<h2>Holiness Christian Academy</h2>
+		<div class="col-md-7 col-md-offset-1">
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ultricies justo eu scelerisque ullamcorper. In vitae mauris dapibus dolor ultrices egestas. Nam hendrerit, tellus ullamcorper hendrerit ultricies, purus mauris faucibus nulla, et pretium libero libero vitae lorem. Morbi finibus felis bibendum, faucibus ante et, tempus lectus. Aliquam ultrices nisl id magna consectetur eleifend. Mauris nec nunc dignissim, malesuada massa non, vestibulum nisi. Nulla consequat enim at congue tempus. In eu nisi nec felis luctus rutrum.</p>
+
+<p>Aenean dignissim euismod porttitor. Nunc suscipit gravida nulla eu luctus. Maecenas non dapibus diam. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Fusce quis massa sit amet ex congue euismod. Mauris et interdum felis, vitae vehicula ex. Nunc vel vestibulum neque. Etiam sodales vehicula nisi eget eleifend. Ut eu sapien est. Nunc at sem tortor. Fusce et nisi nulla.</p>
+			
+		</div>
+		<div class="col-md-3">
+			<?php include 'partials/facebook-notifications.html'; ?>
+		</div>
+	</div>
+	<div class="row">
+		<h2>Contact Us</h2>
+		<div class="col-md-3 col-md-offset-1">
+			<h3>Phone</h3>
+			<p>(443) 553-5477</p>
+			<h3>Address</h3>
+			<p>30 Dogwood Rd
+			<br />Elkton, MD 21921</p>	
+		</div>
+		<div class="col-md-7">
+			
+			<?php include 'partials/map.php'; ?>
+		</div>
+	</div>
+</div>
+<footer>
+	<p>Copyright 2016 Dogwood Baptist Church</p>
+</footer>
 	<?php include('partials/js-links.html'); ?>	
 	
 </body>
