e8503eed697130b9ec3f3a00461adc80938132ac
mjamesderocher <mjamesderocher@gmail.com>
2016-03-07 19:57:58 -0500

    Added content for school page

--- dev/js/application.js
+++ dev/js/application.js
@@ -1,3 +1,5 @@
+//Converts HTML table to Chartist Chart
+
 //tests for SVG support
 function supportsSvg() {
   return document.implementation &&
@@ -11,7 +13,7 @@ function supportsSvg() {
 if (supportsSvg()) {
 
 	var tableToChart=$('.table-to-chart');
-	$( "<div class='ct-chart'></div>" ).insertAfter(tableToChart);
+	$( "<div class='ct-chart ct-minor-seventh'></div>" ).insertAfter(tableToChart);
 	$(tableToChart).css("display", "none"); // We don't need to show the list
 	var chart=$('.ct-chart');
 	var numOfCharts = chart.length; //let's cache the  length
@@ -19,8 +21,8 @@ if (supportsSvg()) {
 		var colIndex = 1;
 	    var currentTable = tableToChart.eq(x);
 	    var currentRow = $("tr", currentTable);
-	    series = [];
-		labels = [];
+	    var series = [];
+		var labels = [];
 	    //adds unique id to each selected element
 	    chart.eq(x).prop('id', 'chartist' + x);
 	    //Get the contents of the contents of first column and send them to series array
@@ -35,21 +37,35 @@ if (supportsSvg()) {
 			var arrayOfThisRow = [];
 			
 			var tableData = (currentRow).find('td:nth-child(' + colIndex + ')');
-			console.log(colIndex);
 			
 		    if (tableData.length > 0) {
 		        tableData.each(function() { arrayOfThisRow.push($(this).text()); });
 		        series.push(arrayOfThisRow);
 		    }
 		});
-		
-		new Chartist.Bar('#chartist'+ x, {
+		var chartAni = new Chartist.Bar('#chartist'+ x, {
 		  labels: labels,
 		  series: series
-		}, {
-		  width: 500,
-		  height: 300
-		});	    
+		});	
+		
+		chartAni.on('draw', function(data) {
+		  if(data.type === 'bar') {
+		    data.element.animate({
+		      y2: {
+		        dur: 2000,
+		        from: data.y1,
+		        to: data.y2,
+		        easing: Chartist.Svg.Easing.easeOutQuint
+		      },
+		      opacity: {
+		        dur: 1000,
+		        from: 0,
+		        to: 1,
+		        easing: Chartist.Svg.Easing.easeOutQuint
+		      }
+		    });
+		  }
+		});    
 	         
 	}	
 }
--- dev/school.php
+++ dev/school.php
@@ -9,6 +9,7 @@
 	<link rel="stylesheet" href="//cdn.jsdelivr.net/chartist.js/latest/chartist.min.css">
 	<link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/css/bootstrap.min.css" rel="stylesheet" integrity="sha256-xWeRKjzyg6bep9D1AsHzUPEWHbWMzlRc84Z0aG+tyms= sha512-mGIRU0bcPaVjr7BceESkC37zD6sEccxE+RJyQABbbKNe83Y68+PyPM5nrE1zvbQZkSHDCJEtnAcodbhlq2/EkQ==" crossorigin="anonymous">
 	<link href="css/application.css" rel="stylesheet">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
 	<link rel="icon" type="image/png" href="favicon.ico">
 	
 	
@@ -27,92 +28,218 @@
 
 		
 
-<div class="container-fluid">
+<div class="container-fluid" id="hca">
 	
 	<div class="row">
-		<?php include('partials/headline.html'); ?>
-	</div>
-	
-	<div class="row">
-		<table class="table-to-chart">
-			<caption>Table 1</caption>
-			<thead>
-				<tr>
-					<th></th>
-					<th>Private School</th>
-					<th>Public School</th>
-				</tr>
-			</thead>
-			<tbody>
-			<tr>
-				<td>Reading</td>
-				<td>91</td>
-				<td>77</td>
-			</tr>
-			<tr>
-				<td>Math</td>
-				<td>83</td>
-				<td>73</td>
-			</tr>
-			<tr>
-				<td>Writing</td>
-				<td>92</td>
-				<td>79</td>
-			</tr>
-			<tr>
-				<td>U.S. History</td>
-				<td>87</td>
-				<td>68</td>
-			</tr>
-			<tr>
-				<td>Geography</td>
-				<td>90</td>
-				<td>73</td>
-			</tr>
-			<tr>
-				<td>Science</td>
-				<td>77</td>
-				<td>64</td>
-			</tr>
-			<tbody>
-		</table>
+		<div class="col-md-10 col-md-offset-1">
+		
+			<img src="/images/original-hca-logo.svg" id="hca-large-logo">
+		
+		    <p>All are invited to explore the facilities and discover the benefits of enrolling your children for Fall 2016.</p>
+		
+		</div>
 	</div>
 	<div class="row">
-		<table class="table-to-chart">
-			<caption>Table 2</caption>
-			<thead>
-				<tr>
-					<th></th>
-					<th>Private School</th>
-					<th>Public School</th>
-				</tr>
-			</thead>
-			<tbody>
-				<tr>
-					<td>School</td>
-					<td>80</td>
-					<td>56</td>
-				</tr>
-				<tr>
-					<td>Teachers</td>
-					<td>76</td>
-					<td>58</td>
-				</tr>
-				<tr>
-					<td>Standard</td>
-					<td>80</td>
-					<td>56</td>
-				</tr>					
-			</tbody>
-		</table>
+		<h2>We Offer Quality Education</h2>
+		<div class="col-md-10 col-md-offset-1">
+			<blockquote>
+				<p>Students don't just get by, they get ahead!</p>
+			</blockquote>
+		
+			<p>
+			Here at Holiness Christian Academy your child will develop a new confidence in his or her learning abilities. We use the <a href="http://www.aceministries.com/curriculum/">A.C.E (Accelerated Christian Education) Curriculum</a> that will fill in the gaps that have been associated with traditional learning methods. Your child will be given the attention and education that will prepare them for higher education with A.C.E.'s concepts of individualized learning. The curriculum is built on <a href="http://www.aceministries.com/curriculum/?content=main&distinctive=individualized">5 laws of learning</a> to help students achieve success:
+			</p>
+		</div>
 	</div>
-
+	<div class="row icons">
+		<div class="col-md-2 col-md-offset-1">
+			<span class="fa fa-user fa-5x"></span>
+			<h3>Individualized</h3> 
+			<p>Students are placed at a level of curriculum where they can best perform and master content.</p>
+		</div>
+		<div class="col-md-2">
+			<span class="fa fa-check-square-o fa-5x"></span>
+			<h3>Goal Setting</h3>
+			<p>Students set reasonable goal that are expected to be achieved in a reasonable amount of time.</p>
+		</div>
+		<div class="col-md-2">
+			<span class="fa fa-fast-forward fa-5x"></span>
+			<h3>Controlled and Motivated</h3>
+			<p>Through guidance and discipline.</p>
+		</div>
+		<div class="col-md-2">
+			<span class="fa fa-line-chart fa-5x"></span>
+			<h3>Learning is Measured</h3>
+			<p>Through PACES, check-ups, and tests.</p>
+		</div>
+		<div class="col-md-2">
+			<span class="fa fa-trophy fa-5x"></span>
+			<h3>Rewards</h3>
+			<p>Students receive rewards for their effort and value of work.</p>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-7 col-md-offset-1">
+			<table class="table-to-chart">
+				<caption>NAEP 12th Grade Report Card</caption>
+				<thead>
+					<tr>
+						<th></th>
+						<th>Private School</th>
+						<th>Public School</th>
+					</tr>
+				</thead>
+				<tbody>
+				<tr>
+					<td>Reading</td>
+					<td>91</td>
+					<td>77</td>
+				</tr>
+				<tr>
+					<td>Math</td>
+					<td>83</td>
+					<td>73</td>
+				</tr>
+				<tr>
+					<td>Writing</td>
+					<td>92</td>
+					<td>79</td>
+				</tr>
+				<tr>
+					<td>U.S. History</td>
+					<td>87</td>
+					<td>68</td>
+				</tr>
+				<tr>
+					<td>Geography</td>
+					<td>90</td>
+					<td>73</td>
+				</tr>
+				<tr>
+					<td>Science</td>
+					<td>77</td>
+					<td>64</td>
+				</tr>
+				<tbody>
+			</table>
+		</div>
+		<div class="col-md-3">
+			<p>The National Center for Education Statistics periodically administers the National Assessment of Educational Progress (NAEP) to test the knowledge and skills of the nation's students in grades 4, 8, and 12. Students in private schools consistently score well above the national average. At all three grades a significantly higher percentage of private school students score at or above the Basic, Proficient, and Advanced levels than public school students. Below are the results from the most recent NAEP report cards.<a href="http://www.capenet.org/facts.html">(source)</a></p>
+		</div>
+	</div>
+	<div class="row">
+		<h2>With Us, You're Kids are Safe</h2>
+		<div class="col-md-10 col-md-offset-1">
+			<p>It seems all the time that we hear on the news of acts of violence and bullying in public schools. Holiness Christian Academy is dedicated to the safety of your children. No outsiders are allowed in the building when school is in session. The ratio of students to teachers is low so that teachers can monitor student behavior for bullying. Our Christian environment encourages courteous interaction between students.</p>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-3 col-md-offset-1">
+			<blockquote>
+				<p>I advise no one to place his child where the Scriptures do not reign paramount. Every institution in which men are not increasingly occupied with the Word of God must become corrupt&#8230;</p>
+				<footer>&mdash;<cite>Martin Luther</cite></footer>
+			</blockquote>
+		</div>
+		<div class="col-md-7">
+			<p>Below is a chart of teachers' perceptions, both in public and private schools, about serious problems in their schools, by control and level of school. <a href="http://nces.ed.gov/programs/digest/d05/tables/dt05_071.asp">(source)</a></p>
+			<table class="table-to-chart">
+			 <tr>
+			  <th></th>
+			  <th>Private school teachers</th>
+			  <th>Public school teachers</th>
+			 </tr>
+			 <tr>
+			  <td>Physical conflicts among Students</td>
+			  <td>1.0</td>
+			  <td>4.8</td> 
+			 </tr>
+			 <tr>
+			  <td>Robbery or theft</td>
+			  <td>0.9</td>
+			  <td>2.4</td>
+			 </tr>
+			 <tr>
+			  <td>Student pregnancy</td>
+			  <td>0.4</td>
+			  <td>3.7</td>
+			 </tr>
+			 <tr>
+			  <td>Student use of alcohol</td>
+			  <td>3.1</td>
+			  <td>7.4</td>
+			 </tr>
+			 <tr>
+			  <td>Student drug abuse</td>
+			  <td>1.8</td>
+			  <td>6.0</td>
+			 </tr>
+			 <tr>
+			  <td>Student possession of weapons</td>
+			  <td>0.3</td>
+			  <td>0.8</td>
+			 </tr>
+			 <tr>
+			  <td>Student apathy</td>
+			  <td>4.3</td>
+			  <td>20.6</td>
+			 </tr>
+			 <tr>
+			  <td>Students come unprepared to learn</td>
+			  <td>4.9</td>
+			  <td>29.5</td>
+			 </tr>
+			</table>
+		</div>
+	</div>
+	<div class="row">
+		<h2>Education with a Biblical Foundation</h2>
+		<div class="col-md-10 col-md-offset-1">
+			<p>From prayer in the morning to afternoon chapels, the students are constantly encouraged to grow in their faith and in their love for God and one another. The A.C.E. curriculum that we use employs Bible memorization and a strong, Christian worldview. We agree that <a href="http://www.worldmag.com/2014/09/why_christian_education">&ldquo;Genuine Christian education seeks to integrate God’s Word into every facet of the curriculum as well as all co-curricular activities.&rdquo;</a></p>
+			<p>We believe that education has to deal with the heart and not just the mind. As <a href="http://www.christianitytoday.com/biblestudies/articles/churchhomeleadership/060719.html">an article</a> in Christianity Today says, &ldquo;Christian education has the authority to speak about more than this visible world, the world that is passing away. Secular education can focus on business and money, matter and molecules, people and issues, but the Christian educator can move beyond to the soul, the human spirit, life after death, the kingdom of God, the return of Christ, the final judgment—things that last.&rdquo;</p>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-3 col-md-offset-1">
+			<blockquote>
+				<p>Bring &#91;children&#93; up in the nurture and admonition of the Lord.</p>
+				<footer>
+					&mdash;<cite>Ephesians 5:6</cite>
+				</footer>
+		</div>
+		<div class="col-md-7">
+			<p>This chart shows the percentage of K-12 students, by type of school, whose parents were very satisfied with aspects of their schools. <a href="http://www.capenet.org/pdf/Outlook389.pdf">(source)</a>
+			<table class="table-to-chart">
+				<caption>Percentage of K-12 students, by type of school, whose parents were very satisfied with aspects of their schools</caption>
+				<thead>
+					<tr>
+						<th></th>
+						<th>Private School</th>
+						<th>Public School</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td>School</td>
+						<td>80</td>
+						<td>56</td>
+					</tr>
+					<tr>
+						<td>Teachers</td>
+						<td>76</td>
+						<td>58</td>
+					</tr>
+					<tr>
+						<td>Standard</td>
+						<td>80</td>
+						<td>56</td>
+					</tr>					
+				</tbody>
+			</table>
+		</div>
+	</div>
 	
 	<div class="row">
 		<?php include('partials/curriculum.html'); ?>		
-	</div>
-	<div class="row">	
-		<?php include('partials/reputation.html'); ?>
 	</div>
 	<div class="row">	
 		<?php include('partials/facebook-notifications.html'); ?>					
